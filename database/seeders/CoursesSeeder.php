e78e07523894038617d8d775e23508f5263423e8
YOUSSEF-YL <youssef.bachar7@gmail.com>
2023-04-06 14:09:42 +0000

    new

---
+++ database/seeders/CoursesSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Courses;
+use Illuminate\Database\Seeder;
+
+class CoursesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Courses::factory()->count(5)->create();
+    }
+}
