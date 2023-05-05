fe228026096e013bd4ef596a8930824d90ce23bf
YOUSSEF BACHAR <youssef.bachar7@gmail.com>
2023-05-05 23:37:25 +0100

    is_admin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();;
+            $table->string('prenom')->nullable();;
+            $table->string('cne')->nullable();;
+            $table->string('cni')->nullable();;
             $table->string('email')->unique();
+            $table->boolean('is_admin')->default('0');
            // $table->string('Appoge')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
