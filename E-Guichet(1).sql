45136781cc5dd51e587c62c90395f6303c842931
YOUSSEF BACHAR <youssef.bachar7@gmail.com>
2023-05-15 23:58:57 +0100

    db

--- E-Guichet(1).sql
+++ E-Guichet(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 06, 2023 at 12:40 AM
+-- Generation Time: May 16, 2023 at 12:58 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -46,7 +46,45 @@ CREATE TABLE `document` (
 --
 
 INSERT INTO `document` (`id`, `nom`, `prenom`, `cne`, `cni`, `appoge`, `scolarite`, `relevedenote`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'youssef', 'blabla', 'bloblo', 'bolb', 'youssef.bachar7@gmail.com', 1, 0, '2023-04-24 16:56:40', '2023-04-24 16:56:40', NULL);
+(8, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:09', '2023-05-14 18:11:09', NULL),
+(9, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:15', '2023-05-14 18:11:15', NULL),
+(10, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:18', '2023-05-14 18:11:18', NULL),
+(11, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:19', '2023-05-14 18:11:19', NULL),
+(12, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:21', '2023-05-14 18:11:21', NULL),
+(13, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:27', '2023-05-14 18:11:27', NULL),
+(14, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:30', '2023-05-14 18:11:30', NULL),
+(15, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:31', '2023-05-14 18:11:31', NULL),
+(16, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:33', '2023-05-14 18:11:33', NULL),
+(17, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:45', '2023-05-14 18:11:45', NULL),
+(18, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:47', '2023-05-14 18:11:47', NULL),
+(19, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:48', '2023-05-14 18:11:48', NULL),
+(20, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:50', '2023-05-14 18:11:50', NULL),
+(21, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:51', '2023-05-14 18:11:51', NULL),
+(22, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:52', '2023-05-14 18:11:52', NULL),
+(23, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:11:54', '2023-05-14 18:11:54', NULL),
+(24, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:00', '2023-05-14 18:12:00', NULL),
+(25, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:03', '2023-05-14 18:12:03', NULL),
+(26, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:06', '2023-05-14 18:12:06', NULL),
+(27, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:07', '2023-05-14 18:12:07', NULL),
+(28, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:09', '2023-05-14 18:12:09', NULL),
+(29, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:10', '2023-05-14 18:12:10', NULL),
+(30, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:11', '2023-05-14 18:12:11', NULL),
+(31, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:13', '2023-05-14 18:12:13', NULL),
+(32, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:14', '2023-05-14 18:12:14', NULL),
+(33, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:15', '2023-05-14 18:12:15', NULL),
+(34, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:16', '2023-05-14 18:12:16', NULL),
+(35, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:22', '2023-05-14 18:12:22', NULL),
+(36, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:23', '2023-05-14 18:12:23', NULL),
+(37, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:24', '2023-05-14 18:12:24', NULL),
+(38, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:25', '2023-05-14 18:12:25', NULL),
+(39, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:26', '2023-05-14 18:12:26', NULL),
+(40, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:27', '2023-05-14 18:12:27', NULL),
+(41, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:29', '2023-05-14 18:12:29', NULL),
+(42, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:31', '2023-05-14 18:12:31', NULL),
+(43, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:32', '2023-05-14 18:12:32', NULL),
+(44, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:34', '2023-05-14 18:12:34', NULL),
+(45, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:35', '2023-05-14 18:12:35', NULL),
+(46, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 1, 1, '2023-05-14 18:12:36', '2023-05-14 18:12:36', NULL);
 
 -- --------------------------------------------------------
 
@@ -72,7 +110,13 @@ CREATE TABLE `email_logs` (
 --
 
 INSERT INTO `email_logs` (`id`, `nome_tudiant`, `prenom_tudiant`, `cne_tudiant`, `cni_tudiant`, `appoge`, `type_email`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'jjj', 'kk', '90u09u0u', 'h8878y9uip', 'o0u089u08', 'Demande de correction de note', NULL, NULL, NULL);
+(10, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 'Demande ajout de module', '2023-05-07 11:59:33', '2023-05-07 11:59:33', NULL),
+(11, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 'Demande de correction de donnees', '2023-05-07 11:59:48', '2023-05-07 11:59:48', NULL),
+(13, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 'Demande de correction de donnees', '2023-05-09 15:53:53', '2023-05-09 15:53:53', NULL),
+(14, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 'Demande de correction de donnees', '2023-05-09 16:20:00', '2023-05-09 16:20:00', NULL),
+(15, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 'Demande de correction de donnees', '2023-05-09 16:23:19', '2023-05-09 16:23:19', NULL),
+(16, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 'Demande de correction de donnees', '2023-05-09 16:26:00', '2023-05-09 16:26:00', NULL),
+(20, 'youssef', 'bachar', 't4358', 't4356', 'youssef.bachar7@gmail.com', 'Demande de correction de note', '2023-05-09 17:30:35', '2023-05-09 17:30:35', NULL);
 
 -- --------------------------------------------------------
 
@@ -120,7 +164,20 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (46, '2019_12_14_000001_create_personal_access_tokens_table', 10),
 (47, '2023_04_24_161740_create_email_logs_table', 10),
 (48, '2023_04_24_165137_create_documents_table', 10),
-(50, '2014_10_12_000000_create_users_table', 11);
+(50, '2014_10_12_000000_create_users_table', 11),
+(51, '2015_09_07_190535_create_languages_table', 12),
+(52, '2015_09_10_124414_alter_languages_table', 12),
+(53, '2023_05_11_174851_create_documents_table', 13),
+(54, '2023_05_11_174853_create_demande_de_corrections_table', 14),
+(55, '2023_05_11_180446_create_demande_de_corrections_table', 15),
+(56, '2023_05_11_180447_create_demande_ajout_de_modules_table', 15),
+(57, '2023_05_11_180448_create_demande_correction_de_donnees_table', 15),
+(61, '2023_05_11_192741_create_demande_de_corrections_table', 16),
+(62, '2023_05_11_192742_create_demande_ajout_de_modules_table', 16),
+(63, '2023_05_11_192743_create_demande_correction_de_donnees_table', 16),
+(64, '2023_05_11_211834_create_demande_de_corrections_table', 17),
+(65, '2023_05_11_211835_create_demande_ajout_de_modules_table', 17),
+(66, '2023_05_11_211836_create_demande_correction_de_donnees_table', 17);
 
 -- --------------------------------------------------------
 
@@ -240,13 +297,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `document`
 --
 ALTER TABLE `document`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `email_logs`
 --
 ALTER TABLE `email_logs`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -258,7 +315,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
