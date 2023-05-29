49b25a9b816632e999a05665231860eaaaa9faa3
mohamed khairoun <102796816+kareou@users.noreply.github.com>
2023-05-29 02:07:59 +0100

    add_language

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,21 +1,27 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
+import {useTranslation} from 'react-i18next'
 import All from '../Components/all'
 
 export default function Dashboard({ auth }: PageProps) {
-    return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-600 leading-tight">Home</h2>}
-        >
-            <Head title="home" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <All user={auth.user}/>
-                </div>
-            </div>
-        </AuthenticatedLayout>
-    );
+    const [t, i18n] = useTranslation();
+
+return (
+  <AuthenticatedLayout
+    user={auth.user}
+    t={t} i18n={i18n}
+    header={<h2 className="font-semibold text-xl text-gray-600 leading-tight">Home</h2>}
+  >
+    <Head title="home" />
+
+    <div className="py-12">
+      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <All user={auth.user} t={t} i18n={i18n} />
+      </div>
+    </div>
+  </AuthenticatedLayout>
+);
+
 }
